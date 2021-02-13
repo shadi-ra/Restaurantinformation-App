c7de67d7eb3aecb4ea45de28e42c667b7aebaf4d
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-02-13 16:43:55 +0330

    ...

--- RestaurantWebApi/Extentions/DependencyExtensions.cs
+++ RestaurantWebApi/Extentions/DependencyExtensions.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Infratructure.EF.Repasitories;
 
 namespace RestaurantWebApi.Extentions
 {
-    public class DependencyExtensions
+    public static class DependencyExtensions
     {
+        public static void AddDependency(this IServiceCollection service)
+        {
+            AddRepositories(service);
+            AddService(service);
+        }
+
+        public static void AddService(IServiceCollection service)
+        {
+      
+
+        }
+
+        public static void AddRepositories(IServiceCollection service)
+        {
+          
+        }
     }
 }
