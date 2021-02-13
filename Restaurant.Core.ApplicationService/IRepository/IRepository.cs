ca3baeab319609a67e5ecc745e4fdbc39c94ca26
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-02-13 16:22:51 +0330

    ...

---
+++ Restaurant.Core.ApplicationService/IRepository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Core.ApplicationService.IRepository
+{
+   public interface IRepository<T>
+    {
+        List<T> GetAll();
+
+        T GetSingel(int id);
+
+        IQueryable<T> GetQuery();
+
+        void Insert(T item);
+
+        T Update(T item);
+
+        Task Delete(int id);
+
+        Task Save();
+    }
+}
--- Restaurant.Infratructure.EF/Repasitories/EfRepository.cs
+++ Restaurant.Infratructure.EF/Repasitories/EfRepository.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Restaurant.Core.ApplicationService.IRepository;
+using Restaurant.Core.Entities;
 
 namespace Restaurant.Infratructure.EF.Repasitories
 {
-    class EfRepository
+    public class EfRepository<T> : IRepository<T> where T :class,IHasIdentity
     {
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
