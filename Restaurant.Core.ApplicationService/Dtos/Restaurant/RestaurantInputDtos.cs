94ffec0ce054ec443cc3c910bd4eafbf3735e8cc
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-02-15 11:52:52 +0330

    ffff

---
+++ Restaurant.Core.ApplicationService/Dtos/Restaurant/RestaurantInputDtos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Core.ApplicationService.Dtos.Restaurant
+{
+   public class RestaurantInputDtos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public int CityId { get; set; }
+        
+    }
+}
