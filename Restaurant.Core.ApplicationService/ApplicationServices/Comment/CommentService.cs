5e23362aa1eef756ea7f17e39cec8c04cb09afba
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-02-16 10:08:16 +0330

    ...

--- Restaurant.Core.ApplicationService/ApplicationServices/Comment/CommentService.cs
+++ Restaurant.Core.ApplicationService/ApplicationServices/Comment/CommentService.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Restaurant.Core.ApplicationService.Dtos.Comment;
+using Restaurant.Core.ApplicationService.IRepository;
 
 namespace Restaurant.Core.ApplicationService.ApplicationServices.Comment
 {
-   public class CommentService:ICommentService
+    public class CommentService : ICommentService
     {
+        private readonly IRepository<Entities.Models.Comment> repository;
+        public async Task<string> Create(CommentInsertInputDto input)
+        {
+            repository.Insert(new Entities.Models.Comment()
+            {
+                comment = input.comment,
+                Rate = input.Rate,
+                UserId = input.UserId,
+                RestaurantId = input.RestaurantId,
+            });
+           await repository.Save();
+            return $"{input.comment} Was Created ";
+        }
+
+        public Task<string> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CommentOutputDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CommentOutputDto> GetSingle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Update(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
